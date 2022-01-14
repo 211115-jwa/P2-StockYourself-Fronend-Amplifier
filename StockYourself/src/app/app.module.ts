aaf93ccd100452b8b02406c125a3d58918245d6a
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-01-14 08:54:13 -0600

    added home component, updated routes

--- StockYourself/src/app/app.module.ts
+++ StockYourself/src/app/app.module.ts
@@ -2,26 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
+
 import { AppComponent } from './app.component';
 import { FetchService } from './services/fetch.service';
-import { YahooAPIService } from './services/yahoo-api.service';
+
 import { UserService } from './services/user.service';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AuthModule
+    AppRoutingModule
   ],
   providers: [
     FetchService,
-    YahooAPIService,
     UserService
   ],
   bootstrap: [AppComponent]
