7a00abe961383468038016d68d81297a43ad3113
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-01-14 13:46:36 -0600

    not working nav

--- StockYourself/src/app/app.module.ts
+++ StockYourself/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,9 +16,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +29,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   providers: [
     FetchService,
     UserService
+  ],
+  exports: [
+    NavComponent
   ],
   bootstrap: [AppComponent]
 })
