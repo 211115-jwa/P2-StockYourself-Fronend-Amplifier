ad9f823269ad132ff6a5b97ff39dbbabec496011
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-01-14 09:36:01 -0600

    fixed a few things in app.module.ts

--- StockYourself/src/app/app.module.ts
+++ StockYourself/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -9,16 +9,19 @@ import { FetchService } from './services/fetch.service';
 import { UserService } from './services/user.service';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     FetchService,
